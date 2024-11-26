afed30925e9e7a14b35c845a13af28173897fdda
columose <colum.ose28@gmail.com>
2024-11-26 12:39:13 +0100

    Update new features

--- functions/HanningWindow.js
+++ functions/HanningWindow.js
@@ -1,40 +1,21 @@
-// Fills samples before segment with zeros
-// Additionally, applies fade-in/out to samples that are 10% less than and greater than the segment window
-export function fHannWin(buffer, startIdx,endIdx){
+//Apply hanning window to noise with fade in and out
+
+export function fHannWin(buffer){
+    const N = Math.floor(buffer.length*0.1); //number samples in fade window
+    const nIdxs = [N, buffer.length-N]
     let winData = buffer.getChannelData(0)
 
     for (let iSamp = 0; iSamp < buffer.length; iSamp++){
-        if(iSamp < startIdx || iSamp > endIdx){
-            winData[iSamp] = 0
+        if(iSamp < nIdxs[0]){
+            winData[iSamp]*= 0.5*(1- Math.cos((Math.PI*iSamp)/(N-1)));//switch to 2 * Math.Pi as well
+        }
+        else if (iSamp > nIdxs[1]){
+            let fadeOutIndex = buffer.length - iSamp;
+            winData[iSamp] *= 0.5*(1-Math.cos((Math.PI*fadeOutIndex)/(N-1)));
         }
         else{
-            winData[iSamp]*=1
+            winData[iSamp] = winData[iSamp]
         }
     }
-    return buffer
-    
-    
-    
-    /*
-    const length = endIdx - startIdx; //Because we're only interested in the selected segment
-    const fadeWindow = Math.floor(length/10);
-    const fadeIdxs = [Math.max(0,startIdx-fadeWindow),Math.min(buffer.length,endIdx+fadeWindow)] // ensure that fade indices exist in noise buffer
-
-    for (let iSamp = 0; iSamp < buffer.length; iSamp++){
-        if (iSamp < fadeIdxs[0] || iSamp > fadeIdxs[1]){
-            winData[iSamp] = 0; //fill samples before and after fade indices with zeros
-        } 
-        else if (iSamp > fadeIdxs[0] && iSamp < startIdx){
-            let hannFactor = 0.5 * (1- Math.cos((2*Math.PI*iSamp)/(fadeWindow-1)));
-            winData[iSamp] *= hannFactor;
-        }
-        else if (iSamp > endIdx && iSamp < fadeIdxs[1]){
-            const fadeOutIndex = iSamp - endIdx; // Relative position in fade-out window
-            let hannFactor = 0.5 * (1 - Math.cos((2 * Math.PI * fadeOutIndex) / (fadeWindow - 1)));
-            winData[iSamp] *= hannFactor;
-        }
-    }
-    const newLen = winData.filter(element => element !== 0).length;
-    */
-    return [buffer,fadeIdxs,newLen];
+ return buffer
 }
--- functions/mixNoise.js
+++ functions/mixNoise.js
@@ -1,13 +1,38 @@
-// This function combines two noise files around the segmented time of interest
+import { fCreateNoiseSample } from "./CreateNoise.js";
 
-export function fMixNoise(noise,maskNoise,startIdx,endIdx){
-    let noiseData = noise.getChannelData(0);
-    let maskData = maskNoise.getChannelData(0);
+// This function combines two noise files around the segmented time of interest.
+// Importantly, the new noise is not repeated
 
-    for (let iSamp = 0; iSamp < ArrayBuffer.length; iSamp++){
-        if (iSamp < startIdx || iSamp > endIdx){
-            noiseData[iSamp] = maskData[iSamp]
+export function fMixNoise(ctx,noise,repeats,startIdx,endIdx){
+    
+    // create an empty buffer that is repeats times longer than noise
+    const chans = noise.numberOfChannels;
+    const sampleRate = noise.sampleRate
+    const length = noise.length * repeats;
+    let buffer = ctx.createBuffer(chans, length, sampleRate) 
+    let bufferData = buffer.getChannelData(0); 
+
+    // repeating noise data is constant
+    const noiseData = noise.getChannelData(0);
+
+    // Fill buffer with new and repeated noise
+    let bufferIdx = 0; // create variable to index buffer samples
+    for (let iRep = 0; iRep < repeats; iRep++){
+
+        // Create new noise in each iteration so this noise pattern cannot be detected as a repeat
+        let newData = fCreateNoiseSample(ctx, noise.duration).getChannelData(0);
+
+        // Fill new buffer with a mix of repeated and non-repeated noise
+        for (let iSamp = 0; iSamp < noise.length; iSamp++){
+            if (iSamp < startIdx || iSamp > endIdx){
+                bufferData[iSamp + bufferIdx] = newData[iSamp]; 
+            } 
+            else{
+                bufferData[iSamp + bufferIdx] = noiseData[iSamp];
+            }
         }
+        // change buffer index for loop number 2 to log repeat
+        bufferIdx += noise.length;
     }
-    return noise
+    return buffer;
 }
