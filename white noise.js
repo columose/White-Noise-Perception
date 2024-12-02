c0a4ea68d767369558da7c072ac6770167fba46d
columose <colum.ose28@gmail.com>
2024-12-02 16:53:46 +0100

    shift height

--- white noise.js
+++ white noise.js
@@ -94,7 +94,7 @@ window.onload = () => {
         if (event.key === 'h' && shifted === false){
             rectHeight += freqChange;
         }
-        else if (state === 'height' && shifted === true){
+        else if (event.key === 'h' && shifted === true){
             rectHeight = Math.max(freqChange, rectHeight - freqChange);
         }
         // Change rectangle width
@@ -117,7 +117,7 @@ window.onload = () => {
         else if (event.key === 'ArrowRight'){
             zeroX = Math.min(filterCanvas.width - rectWidth, zeroX + timeChange);
         }
-        
+
         //update coordinates after interaction so they can be used to calculate time-freq cut offs
         updateCoords(); 
         drawRectangle();
