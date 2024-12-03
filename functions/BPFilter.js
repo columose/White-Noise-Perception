b6649448da8bc09d57f7ddb03b685e1dae25e48f
columose <colum.ose28@gmail.com>
2024-12-03 15:24:36 +0100

    nice changes, especially to filter

--- functions/BPFilter.js
+++ functions/BPFilter.js
@@ -2,13 +2,15 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Advanced_techniques#the_noise_%E2%80%94_random_noise_buffer_with_a_biquad_filter
 
 //Create filter
-export function fcreateBPFilter (ctx,Q, low, high){
+export function fcreateBPFilter (ctx,low, high){
     const filter = ctx.createBiquadFilter()
 
     //Define filter and cutoffs
-    const centreFreq = Math.sqrt(low * high)
+    const BW = high-low;
+    const centreFreq = (low + high)/2;
     filter.type = 'bandpass'
     filter.frequency.value = centreFreq;
+    let Q = centreFreq/BW;
     filter.Q.value = Q;
     return filter;
 }
